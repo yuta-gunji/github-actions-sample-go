5d6bf720be817c63fbb77b0b1e32cb61db93bd0b
Yuta Gunji <yuta.g05@gmail.com>
2024-07-25 07:24:23 +0900

    fix test

--- go/excellent/main_test.go
+++ go/excellent/main_test.go
@@ -3,8 +3,13 @@ package main
 import "testing"
 
 func TestEvenOrOdd(t *testing.T) {
-	result := EvenOrOdd(10)
-	if result != "even" {
-		t.Errorf("expected: even, actual: %s", result)
+	result1 := EvenOrOdd(10)
+	if result1 != "even" {
+		t.Errorf("expected: even, actual: %s", result1)
+	}
+
+	result2 := EvenOrOdd(11)
+	if result2 != "odd" {
+		t.Errorf("expected: odd, actual: %s", result2)
 	}
 }
